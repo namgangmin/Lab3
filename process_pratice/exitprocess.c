015a3d65da0d92dc05a73c85ef7e8b8dc069422f
root <root@DESKTOP-G6RRIL6>
2023-11-26 21:34:37 +0900

    프로세스 관련 함수 실습

---
+++ process_pratice/exitprocess.c
@@ -0,0 +1,13 @@
+/* exitprocess.c */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int exit_status;
+	printf("enter exit status: ");
+	scanf("%d", &exit_status);
+	exit(exit_status);
+}
+
